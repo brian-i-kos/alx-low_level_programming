a6ff2797047bafa24903699285c7660b6c43abc6
oktarikosgey <brianchiri@yahoo.com>
2022-04-08 15:47:05 -0700

    final

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+
 /**
- * main - prints "Programming is like building a multilingual puzzle"
+ * main - Entry point
+ * prints "Programming is like building a multilingual puzzle"
  * Return : 0 if exited properly, non zero otherwise
  */
 int main(void)
