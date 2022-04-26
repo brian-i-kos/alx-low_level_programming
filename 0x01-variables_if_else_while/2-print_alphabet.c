c6b553c3f1c68ab0c15e0b2a5289b0d7fb43da0f
oktarikosgey <brianchiri@yahoo.com>
2022-04-26 10:32:42 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints alphabet in lower case
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+	{
+		putchar(letters);
+	}
+	putchar('\n');
+	return(0);
+}
