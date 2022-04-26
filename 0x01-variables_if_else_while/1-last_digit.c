b6d80ad05dea7f6a3716b91efecd4edfb7616676
oktarikosgey <brianchiri@yahoo.com>
2022-04-26 10:23:37 -0700

    g

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,7 +9,7 @@
  */
 
 int main(void)
-{	
+{
 	int n;
 
 	srand(time(0));
@@ -22,4 +22,4 @@ int main(void)
 	else if (n % 10 < 6 && n % 10 != 0)
 		printf("and is less than 6 and not 0\n");
 	return (0);
-}	
+}
