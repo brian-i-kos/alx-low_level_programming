c2992e7797e7038c3f1ec407c4aa819e6cca67c2
oktarikosgey <brianchiri@yahoo.com>
2022-04-26 12:12:52 -0700

    3 combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints combinations of 3 digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				if (i != j && i < j && j < k)
+				{
+					putchar(j + 48);
+					putchar(i + 48);
+					putchar(j + 48);
+					putchar(k + 48);
+					if (!((i == 7) && (j == 8) && (k == 9)))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
+
