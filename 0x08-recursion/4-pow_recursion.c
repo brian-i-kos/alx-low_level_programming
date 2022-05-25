e49536b2585d44253c52eb02c1cc4bbf514b80d4
brian-i-kos <brianchiri@yahoo.com>
2022-05-25 11:32:24 -0700

    pow recursion changes

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,28 +1,22 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - main function
- * @n: int n
- * Return: int
+ * _pow_recursion - pow recursion
+ * @x: integer params
+ * @y: integer paramtr
+ * Return: recursion
  */
-int _sqrt_recursion(int n)
-{
-	return (_sqrt(n, 1));
-}
 
-/**
- * _sqrt - _sqrt_recursion
- * @n: integer paramtr
- * @i: integer parameter
- * Return: sqrt
- */
-int _sqrt(int n, int i)
+
+int _pow_recursion(int x, int y)
 {
-	if (n < 0)
+	if (y < 0)
+	{
 		return (-1);
-	if ((i * i) > n)
-		return (-1);
-	if (i * i == n)
-		return (i);
-	return (_sqrt(n, i + 1));
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
 }
